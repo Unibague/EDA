5b2941f69ebd0789dc19b0f4832cec275893b5b9
JEstebanGnz <juanes01.gm@outlook.com>
2024-03-06 09:45:19 -0500

    pdfs names fixed

--- resources/views/commitmentReport.blade.php
+++ resources/views/commitmentReport.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Reporte por docente ALETHEIA</title>
+    <title>Reporte por funcionario: EDA</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></head>
 <body class="mx-5 my-5">
 
