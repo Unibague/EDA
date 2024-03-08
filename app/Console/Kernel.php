3ac7b37181e4046b9841deae3968f615c5759aae
JEstebanGnz <juanes01.gm@outlook.com>
2024-03-08 17:42:50 -0500

    crontab test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -28,6 +29,9 @@ class Kernel extends ConsoleKernel
         $schedule->command('commitmentReminder:send')->dailyAt('10:00');
         $schedule->command('aggregate_grades:update')->everyThirtyMinutes();
         $schedule->command('assessmentReminder:send')->dailyAt('08:00');
+        $schedule->call(function () {
+            Log::info("Crontab working correctly");
+        })->daily();
 //        $schedule->command('assessmentReminder:send')->cron('48 5 8 3 *');
     }
 
